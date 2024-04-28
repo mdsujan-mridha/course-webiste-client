d8466a2f0a8ca3fadb1e1d2f67534f60eced9f6c
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-04-28 14:29:01 +0600

    v1 client site

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,8 @@
 
 
 import React from 'react';
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+
 
 const About = () => {
     return (
@@ -13,16 +15,80 @@ const About = () => {
             <div>
                 <h1 className='text-white font-bold text-xl px-12 pt-16 pb-12'> At Science Pathshala, we are dedicated to empowering students to excel in physics and mathematics at the HSC level. Our coaching center is founded on the principles of quality education, personalized attention, and a passion for learning. </h1>
             </div>
-            <div className='px-12 py-12 flex justify-between items-center'>
-                <div>
+            <div className='px-12 py-12 flex justify-between items-center flex-col lg:flex-row'>
+                <div className='w-full lg:w-8/12'>
                     <h1 className='text-3xl font-bold text-white'> Our Mission </h1>
-                    <p className='py-10'> Our mission at Science Pathshala is to provide high-quality education in physics and mathematics to HSC level students. We aim to cultivate a deep understanding of these subjects, foster critical thinking skills, and prepare students for academic success. </p>
+                    <p className='py-10'> At Science Pathshala, our mission is to provide high-quality education in physics and mathematics to HSC level students. We are dedicated to nurturing a love for learning, fostering intellectual curiosity, and empowering students to achieve academic excellence. </p>
+                    <h3 className='text-xl font-bold text-white'> Our mission is guided by several core principles: </h3>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Excellence in Education: </p>
+                        <p> We are committed to delivering excellence in education by providing students with comprehensive, well-structured courses that cover all aspects of physics and mathematics at the HSC level. Our goal is to equip students with the knowledge, skills, and confidence they need to succeed academically. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Personalized Attention </p>
+                        <p> We believe in the power of personalized learning. Our experienced faculty members take the time to understand each student's unique strengths, weaknesses, and learning styles, allowing us to tailor our teaching methods to meet their individual needs. Whether students need additional support in certain areas or want to explore advanced topics, we are here to provide guidance and assistance every step of the way. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Holistic Development: </p>
+                        <p> Beyond academic success, we are dedicated to fostering holistic development in our students. In addition to mastering physics and mathematics, we emphasize the importance of critical thinking, problem-solving, and effective communication skills. Our goal is to prepare students not only for their HSC examinations but also for success in higher education and beyond. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Passion for Learning: </p>
+                        <p> At Science Pathshala, we are passionate about learning and are committed to instilling that same passion in our students. We believe that learning should be engaging, exciting, and rewarding. Through interactive lectures, hands-on activities, and real-world applications, we strive to ignite students' curiosity and inspire a lifelong love for learning. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Community Engagement:  </p>
+                        <p> We recognize the importance of community engagement in education. We actively involve parents, teachers, and students in the learning process, fostering a supportive and collaborative learning environment. By working together, we can create positive educational experiences that benefit everyone involved. </p>
+                    </div>
+
                 </div>
                 <div className='w-full lg:w-1/2'>
                     <img src="https://i.ibb.co/qN9XMwF/Untitled-design.png" alt="mission" className='w-full' />
                 </div>
-            </div>
 
+            </div>
+            <p className='px-12 text-center text-xl'>Join us at Science Pathshala as we work towards our mission of empowering students to excel in physics and mathematics. Together, we can inspire a new generation of learners and make a positive impact on the world through education.</p>
+
+
+
+            {/* vision  */}
+
+            <div className='px-12 py-12 flex justify-between items-center flex-col lg:flex-row-reverse'>
+                <div className='w-full lg:w-8/12'>
+                    <h1 className='text-3xl font-bold text-white text-center'> Our Mission </h1>
+                    <p className='py-10'> At Science Pathshala, our mission is to provide high-quality education in physics and mathematics to HSC level students. We are dedicated to nurturing a love for learning, fostering intellectual curiosity, and empowering students to achieve academic excellence. </p>
+                    <h3 className='text-xl font-bold text-white'> Our mission is guided by several core principles: </h3>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Excellence in Education: </p>
+                        <p> We are committed to delivering excellence in education by providing students with comprehensive, well-structured courses that cover all aspects of physics and mathematics at the HSC level. Our goal is to equip students with the knowledge, skills, and confidence they need to succeed academically. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Personalized Attention </p>
+                        <p> We believe in the power of personalized learning. Our experienced faculty members take the time to understand each student's unique strengths, weaknesses, and learning styles, allowing us to tailor our teaching methods to meet their individual needs. Whether students need additional support in certain areas or want to explore advanced topics, we are here to provide guidance and assistance every step of the way. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Holistic Development: </p>
+                        <p> Beyond academic success, we are dedicated to fostering holistic development in our students. In addition to mastering physics and mathematics, we emphasize the importance of critical thinking, problem-solving, and effective communication skills. Our goal is to prepare students not only for their HSC examinations but also for success in higher education and beyond. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Passion for Learning: </p>
+                        <p> At Science Pathshala, we are passionate about learning and are committed to instilling that same passion in our students. We believe that learning should be engaging, exciting, and rewarding. Through interactive lectures, hands-on activities, and real-world applications, we strive to ignite students' curiosity and inspire a lifelong love for learning. </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-5 items-center py-5 font-bold'> <IoCheckmarkCircleOutline className='text-2xl font-bold text-cyan-400' /> Community Engagement:  </p>
+                        <p> We recognize the importance of community engagement in education. We actively involve parents, teachers, and students in the learning process, fostering a supportive and collaborative learning environment. By working together, we can create positive educational experiences that benefit everyone involved. </p>
+                    </div>
+
+                </div>
+                <div className='w-full lg:w-1/2'>
+                    <img src="https://i.ibb.co/6yTtn3z/Untitled-design-2.png" alt="mission" className='w-full' />
+                </div>
+
+            </div>
+            <div className='px-16 py-12'>
+                <h1 className='text-center font-extrabold lg:text-5xl'> Our Approach </h1>
+                <p className='text-center py-12'> At Science Pathshala, we believe in a student-centric approach to education. Our experienced faculty members are dedicated to crafting engaging lessons tailored to the individual needs of each student. Through interactive lectures, hands-on activities, and personalized attention, we aim to empower our students to reach their full potential. </p>
+            </div>
         </div>
     );
 };
